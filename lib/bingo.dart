eae770a5b980f899e03619062039faa33c806f67
Extauren <theodoregozard@protonmail.com>
2023-06-13 18:15:32 +0900

    Add bottom navigation bar, home bingo statistic and settings pages

---
+++ lib/bingo.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Bingo extends StatelessWidget {
+  const Bingo({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Bingo"));
+  }
+}
---
+++ lib/navigation_bar.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+import 'bingo.dart';
+import 'statistic.dart';
+import 'settings.dart';
+
+class NavigationBarApp extends StatelessWidget {
+  const NavigationBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: NavigationBar(),
+    );
+  }
+}
+
+class NavigationBar extends StatefulWidget {
+  const NavigationBar({super.key});
+
+  @override
+  State<NavigationBar> createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Home(),
+    Bingo(),
+    Statistic(),
+    Settings(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+            canvasColor: Colors.orange,
+            primaryColor: Colors.white,
+            textTheme: Theme.of(context)
+                .textTheme
+                .copyWith(caption: TextStyle(color: Colors.grey))),
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.games_outlined),
+              label: 'Bingo',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.graphic_eq),
+              label: 'Statistics',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.black,
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+}
