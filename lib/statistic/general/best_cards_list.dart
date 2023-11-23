7b536ecf9567860b028217b9bae45f9a583df3ed
Extauren <theodoregozard@protonmail.com>
2023-11-23 15:52:06 +0100

    Add BestCardData class

--- lib/statistic/general/best_cards_list.dart
+++ lib/statistic/general/best_cards_list.dart
@@ -2,6 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:plaktago/components/table.dart';
 import 'package:plaktago/data_class/save_game.dart';
 
+class CardListData {
+  final int pourcentage;
+  final String cardName;
+  final int nbPlayed;
+  final int nbCheck;
+
+  CardListData(
+      {required this.pourcentage,
+      required this.cardName,
+      required this.nbPlayed,
+      required this.nbCheck});
+}
+
 class BestCardsList extends StatefulWidget {
   final List<CardList> cardList;
   final int nbRows;
@@ -23,28 +36,35 @@ class BestCardsList extends StatefulWidget {
 class _BestCardsList extends State<BestCardsList> {
   List<String> headerText = ["Nom", "Joué", "Coché", "%"];
   List<List<String>> rowsText = [[], [], [], []];
-  List<String> pourcentages = [];
+  List<CardListData> cardListData = [];
 
   @override
   void initState() {
     super.initState();
     getPourcentages();
-    rowsText[0] = widget.cardList.map((e) => e.cardName.toString()).toList();
-    rowsText[1] = widget.cardList.map((e) => e.nbPlayed.toString()).toList();
-    rowsText[2] = widget.cardList.map((e) => e.nbCheck.toString()).toList();
-    rowsText[3] = pourcentages;
+    rowsText[0] = cardListData.map((e) => e.cardName).toList();
+    rowsText[1] = cardListData.map((e) => e.nbPlayed.toString()).toList();
+    rowsText[2] = cardListData.map((e) => e.nbCheck.toString()).toList();
+    rowsText[3] = cardListData.map((e) => e.pourcentage.toString()).toList();
   }
 
   void getPourcentages() {
     double pourcentage = 0.0;
-
     for (int it = 0; it < widget.cardList.length; it++) {
-      pourcentage = widget.cardList.elementAt(it).nbCheck /
-          widget.cardList.elementAt(it).nbPlayed *
-          100;
-      pourcentages.add(pourcentage.round().toString());
+      if (widget.cardList.elementAt(it).nbPlayed == 0) {
+        pourcentage = 0;
+      } else {
+        pourcentage = widget.cardList.elementAt(it).nbCheck /
+            widget.cardList.elementAt(it).nbPlayed *
+            100;
+      }
+      cardListData.add(CardListData(
+          pourcentage: pourcentage.round(),
+          cardName: widget.cardList.elementAt(it).cardName,
+          nbPlayed: widget.cardList.elementAt(it).nbPlayed,
+          nbCheck: widget.cardList.elementAt(it).nbPlayed));
     }
-    pourcentages.sort((a, b) => b.compareTo(a));
+    cardListData.sort((a, b) => b.pourcentage.compareTo(a.pourcentage));
   }
 
   @override
