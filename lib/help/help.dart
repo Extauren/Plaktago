4917f36573d9e827e83cb182a45077bfdafd25b0
Extauren <theodoregozard@protonmail.com>
2023-11-28 00:26:24 +0100

    Remove unused import

--- lib/help/help.dart
+++ lib/help/help.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:data_table_2/data_table_2.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:plaktago/components/app_bar.dart';
 import 'package:plaktago/game/board/card_name.dart';
