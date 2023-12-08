98958102ae09aa32b3c194ee33e8cdc6ef3e3aea
Extauren <theodoregozard@protonmail.com>
2023-12-08 18:47:24 +0100

    Add color optional parameter

--- lib/utils/get_icon.dart
+++ lib/utils/get_icon.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:plaktago/home/bingo_type_button.dart';
 
-Widget getIcon(final BingoType bingoType) {
+Widget getIcon(final BingoType bingoType, [final Color? color]) {
   late Widget icon;
   if (bingoType == BingoType.kta) {
-    icon = Icon(FontAwesomeIcons.dungeon, color: Colors.white);
+    icon = Icon(FontAwesomeIcons.dungeon, color: color ?? Colors.white);
   }
   if (bingoType == BingoType.exploration) {
-    icon = Icon(FontAwesomeIcons.personWalking, color: Colors.white);
+    icon = Icon(FontAwesomeIcons.personWalking, color: color ?? Colors.white);
   }
   if (bingoType == BingoType.plaque) {
     icon = ImageIcon(
@@ -16,7 +16,7 @@ Widget getIcon(final BingoType bingoType) {
           'assets/manhole_cover.png',
         ),
         size: 27,
-        color: Colors.white);
+        color: color ?? Colors.white);
   }
   return Container(margin: EdgeInsets.only(right: 15), child: icon);
 }
