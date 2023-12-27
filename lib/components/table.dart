586dd1c7d7f9f76976e3983d5739e1eeb6ac462d
Extauren <theodoregozard@protonmail.com>
2023-12-27 16:34:43 +0100

    Fix datatable2 collumn size

--- lib/components/table.dart
+++ lib/components/table.dart
@@ -18,43 +18,40 @@ class PTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(0),
-      child: DataTable2(
-          showBottomBorder: false,
-          border: TableBorder.all(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
-              color: Theme.of(context).colorScheme.secondary),
-          horizontalMargin: 12,
-          headingRowHeight: headingRowHeight,
-          dataRowHeight: dataRowHeight,
-          headingTextStyle: TextStyle(fontWeight: FontWeight.bold),
-          columns: [
-            DataColumn2(
-                label: Center(child: Text(headerText[0])), fixedWidth: 95),
-            DataColumn2(label: Text(headerText[1]), fixedWidth: 94),
-            DataColumn2(label: Text(headerText[2]), fixedWidth: 96),
-            DataColumn2(label: Text(headerText[3]), fixedWidth: 55),
-          ],
-          rows: List<DataRow>.generate(
-              nbRows,
-              (index) => DataRow(cells: [
-                    DataCell(Center(
-                        child: Text(
-                      rowsText[0][index],
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 2,
-                    ))),
-                    DataCell(Center(
-                        child: Text(
-                      rowsText[1][index],
-                    ))),
-                    DataCell(Center(child: Text(rowsText[2][index]))),
-                    DataCell(Text(
-                      rowsText[3][index],
-                    ))
-                  ]))),
+    return DataTable2(
+      showBottomBorder: false,
+      border: TableBorder.all(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+          color: Theme.of(context).colorScheme.secondary),
+      horizontalMargin: 12,
+      headingRowHeight: headingRowHeight,
+      dataRowHeight: dataRowHeight,
+      headingTextStyle: TextStyle(fontWeight: FontWeight.bold),
+      columns: [
+        DataColumn2(label: Center(child: Text(headerText[0])), fixedWidth: 95),
+        DataColumn2(label: Text(headerText[1]), fixedWidth: 94),
+        DataColumn2(label: Text(headerText[2]), fixedWidth: 96),
+        DataColumn2(label: Text(headerText[3]), fixedWidth: 70),
+      ],
+      rows: List<DataRow>.generate(
+          nbRows,
+          (index) => DataRow(cells: [
+                DataCell(Center(
+                    child: Text(
+                  rowsText[0][index],
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ))),
+                DataCell(Center(
+                    child: Text(
+                  rowsText[1][index],
+                ))),
+                DataCell(Center(child: Text(rowsText[2][index]))),
+                DataCell(Text(
+                  rowsText[3][index],
+                ))
+              ])),
     );
   }
 }
